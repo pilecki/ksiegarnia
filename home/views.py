d666bc0e1807623d51a1fd5fb186733ea90c21ab
pilecki <mlaskowski@onet.pl>
2020-07-12 01:09:55 +0000

    Corrected responsivness on mobile devices; category new books added to the front page

--- home/views.py
+++ home/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
 
@@ -6,4 +7,14 @@ from django.shortcuts import render
 def index(request):
     """ A view for the index page """
 
-    return render(request, 'home/index.html')
+    products = Product.objects.filter(genre='1')[:3]
+    newest_products = Product.objects.order_by('-pub_date')[:4]
+
+
+    context = {
+        'products': products,
+        'newest_products': newest_products,
+
+    }
+
+    return render(request, 'home/index.html', context)
--- products/views.py
+++ products/views.py
@@ -1,17 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 # Create your views here.
 
+def product_detail(request, product_id):
+    """ A view to show individual product details """
 
-
-def all_products(request):
-    """ A view to show all products """
-
-    products = Product.objects.all()
+    product = get_object_or_404(Product, pk=product_id)
 
     context = {
-        'products': products,
+        'product': product,
     }
 
-    return render(request, 'products/products.html', context)
+    return render(request, 'products/product_detail.html', context)
