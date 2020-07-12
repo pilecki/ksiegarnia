d64f07ec54669a515ae57dcc1039e2be26f3a5c8
pilecki <mlaskowski@onet.pl>
2020-07-12 04:08:52 +0000

    Setup view and a page for newest books

--- home/views.py
+++ home/views.py
@@ -11,6 +11,7 @@ def index(request):
     newest_products = Product.objects.order_by('-pub_date')[:4]
 
 
+
     context = {
         'products': products,
         'newest_products': newest_products,
@@ -18,3 +19,17 @@ def index(request):
     }
 
     return render(request, 'home/index.html', context)
+
+
+def newest(request):
+    """ A page  showing the last 12 books added"""
+
+    newest_products = Product.objects.order_by('-pub_date')[:11]
+    context = {
+
+        'newest_products': newest_products,
+
+    }
+
+
+    return render(request, 'home/newest.html', context)
