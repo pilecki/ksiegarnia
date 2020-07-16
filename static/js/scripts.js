30ac835adae9125e2b5c761409a983291f3db8f5
pilecki <mlaskowski@onet.pl>
2020-07-16 00:20:11 +0000

    Adden +/- buttons to input quantity field

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -10,3 +10,44 @@ function closeNav() {
   document.body.style.backgroundColor = "white";
 }
 
+// Variables for up and down quantity buttons and input field 
+
+let countDownBtn = document.querySelector('.decrement-qty');
+let input = document.querySelector('.qty_input');
+let countUpBtn = document.querySelector('.increment-qty');
+
+// Function to check input value
+
+const checkInputValue = function (){
+
+    let num3 = (input.value = parseInt(input.value))
+    
+    countDownBtn.setAttribute('disabled', '');
+
+    if (num3 > 1 ) {
+       countDownBtn.removeAttribute('disabled', ''); 
+    }
+    if ( num3 >= 98 ){
+        countUpBtn.setAttribute('disabled', '');
+    }
+    if (num3 < 99 ) {
+       countUpBtn.removeAttribute('disabled', ''); 
+    }
+} 
+
+
+// Incerement quantity
+   
+countUpBtn.addEventListener('click', (event) => {
+event.preventDefault()
+input.value = parseInt(input.value) +1
+checkInputValue();
+});
+
+// Decrement quantity
+
+countDownBtn.addEventListener('click', (event) => {
+    event.preventDefault()
+    input.value = parseInt(input.value) - 1
+  checkInputValue();
+}); 
