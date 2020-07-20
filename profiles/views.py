8eca61a552475891ec03909c3c410dcdece7ce00
pilecki <mlaskowski@onet.pl>
2020-07-20 02:35:40 +0000

    Added order history and updated checkout views

--- profiles/views.py
+++ profiles/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 
 from .models import UserProfile
 from .forms import UserProfileForm
+from checkout.models import Order
 
 def profile(request):
     """ Display the user's profile. """
@@ -22,3 +23,16 @@ def profile(request):
     }
 
     return render(request, template, context)
+
+
+def order_history(request, order_number):
+    order = get_object_or_404(Order, order_number=order_number)
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+        'from_profile': True,
+    }
+
+    return render(request, template, context)
+
