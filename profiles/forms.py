806c05fc278f869f96798921befdbd56a69fb6da
pilecki <mlaskowski@onet.pl>
2020-07-29 16:04:57 +0000

    Changed buttons and border colour in form  in profile; added border to cards

--- profiles/forms.py
+++ profiles/forms.py
@@ -30,5 +30,5 @@ class UserProfileForm(forms.ModelForm):
             else:
                 placeholder = placeholders[field]
             self.fields[field].widget.attrs['placeholder'] = placeholder
-            self.fields[field].widget.attrs['class'] = 'border border-warning'
+            self.fields[field].widget.attrs['class'] = 'default-border'
             self.fields[field].label = False
